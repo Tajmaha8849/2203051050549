690f37cc0f099815ba2992c4050d61de7b2e2fee
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2025-06-23 15:49:33 +0530

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import './Dashboard.css' // 🔗 Link the CSS file
+import './Dashboard.css' 
 
 const Dashboard = () => {
   const [originalUrl, setOriginalUrl] = useState('')
